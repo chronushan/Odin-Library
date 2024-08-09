898b9081f748e43728e18587ee996fb70069f6d8
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-08-09 15:51:51 +0900

    Add basic functions on javascript file

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ function Book(author, title, pages, status) {
 	this.author = author;
 	this.title = title;
 	this.pages = pages;
-	this.read = read;
+	this.status = status;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
 	// Add book to library array
-	myLibrary.push();
+	myLibrary.push(book);
 }
+
+function displayBookToLibrary(myLibrary) {
+	for (const books of myLibrary) {
+		for (const prop in books) {
+			console.log(books[prop]);
+		}
+	}
+}
+
+const Harry = new Book("Harry Potter", "J.K Rowling", 300, "read");
+
+addBookToLibrary(Harry);
+
+displayBookToLibrary(myLibrary);
