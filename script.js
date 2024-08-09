1afa058b28f29ddf0fc9654af8955182244f4a28
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-08-09 14:53:28 +0900

    Add sidebar layout

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(author, title, pages, status) {
+	// the constructor...
+	this.author = author;
+	this.title = title;
+	this.pages = pages;
+	this.read = read;
+}
+
+function addBookToLibrary() {
+	// do stuff here
+	myLibrary.push();
+}
